b1f682571a26205af820db177dfaf01f30afac68
leiz2192 <zhanglei2192@126.com>
2021-09-05 00:03:59 +0800

    refactor: delete some unused codes

--- src/main.py
+++ src/main.py
@@ -13,10 +13,6 @@ def get_lazy_time():
     suffix = "{}月{} {}{}年".format(today.cn_month, today.cn_day, today.gz_year, today.animal)
     return "{} {}".format(prefix, suffix)
 
-def update_lazy_time(window):
-    while True:
-        time.sleep(10)
-        window["lazy_time"].update(get_lazy_time())
 
 class UpdateLazyTime(threading.Thread):
     def __init__(self, window):
@@ -34,19 +30,17 @@ class UpdateLazyTime(threading.Thread):
     def stop(self):
         self._stop = True
 
-def main():
-    print("Hello LazyTime")
 
+def main():
     sg.theme('Reddit')
     layout = [[sg.Text(get_lazy_time(), font=("Microsoft YaHei", 10), key="lazy_time", enable_events=True)]]
-    window = sg.Window('', layout, no_titlebar=True, grab_anywhere=True, keep_on_top=True, margins=(0,0), alpha_channel=0.6)
+    window = sg.Window('', layout, no_titlebar=True, grab_anywhere=True, keep_on_top=True, margins=(0,0), alpha_channel=0.75)
     uthread = UpdateLazyTime(window)
     uthread.start()
 
     last_time = None
     while True:
-        event, values = window.read()
-        print(datetime.datetime.now().isoformat(), event, values)
+        event, _ = window.read()
         if event != "lazy_time":
             continue
         if last_time and (datetime.datetime.now() - last_time).seconds < 1:
